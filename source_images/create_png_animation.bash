b9a913dc1b4b0702b2f348338d15fff40923c5aa
Tim Schäfer <ts+code@rcmd.org>
2025-03-17 13:41:32 +0100

    NEW: better doc for script

--- source_images/create_png_animation.bash
+++ source_images/create_png_animation.bash
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Generate the animated APNG files from the individual frames.
+#
+# This script should be run from the 'source_images' directory.
+# It requires ffmpeg to be installed. Use the following command to install it:
+#
+#  sudo apt-get install ffmpeg
+#
+#
+
+output_dir="../static"
+
+echo "Generating animated PNG files from individual frames."
+echo "Will save output to directory: ${output_dir}"
+
+if [ ! -d "$output_dir" ]; then
+    echo "Please run this script from the 'source_images' directory and make sure the output directory exists."
+    exit 1
+fi
 
 for color in "gray" "green" "orange" "purple" "yellow"; do
     echo "Converting ${color} bird frames to to bird${color}_animated.png"
-    ffmpeg -y -framerate 2 -i bird${color}_frame%d.png -plays 0 -vf "fps=2" -f apng "bird${color}_animated.png"
-    #ffmpeg -framerate 2 -i "bird${color}_frame%03d.png" -vf "fps=2" -plays 0 "bird${color}_animated.png"
+    ffmpeg -y -framerate 2 -i bird${color}_frame%d.png -plays 0 -vf "fps=2" -f apng "${output_dir}/bird${color}_animated.png"
 done
+
+echo "Script done. Check output above for any errors."
+
