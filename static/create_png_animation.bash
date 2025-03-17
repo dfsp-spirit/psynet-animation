559e0ab31bd60eb581abb8edaac3dc82a09bd04a
Tim Schäfer <ts+code@rcmd.org>
2025-03-17 13:07:22 +0100

    CHG: update shell script

--- static/create_png_animation.bash
+++ static/create_png_animation.bash
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for color in {"gray","red","green","blue","yellow","purple","cyan","orange","brown","pink"}; do
+for color in "gray" "green" "orange" "purple" "yellow"; do
     echo "Converting ${color} bird frames to to bird${color}_animated.png"
     ffmpeg -framerate 2 -i bird${color}_frame%d.png -plays 0 bird${color}_animated.png
 done
