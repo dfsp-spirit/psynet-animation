71cac6d33a36b5b0d2d37318f0524f43cf44f693
Tim Schäfer <ts+code@rcmd.org>
2025-03-17 13:32:50 +0100

    CHG: overwrite with ffmpeg without prompt

--- static/create_png_animation.bash
+++ static/create_png_animation.bash
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#for color in "gray" "green" "orange" "purple" "yellow"; do
-for color in "yellow"; do
+for color in "gray" "green" "orange" "purple" "yellow"; do
     echo "Converting ${color} bird frames to to bird${color}_animated.png"
-    ffmpeg -framerate 2 -i bird${color}_frame%d.png -plays 0 -vf "fps=2" -f apng "bird${color}_animated.png"
+    ffmpeg -y -framerate 2 -i bird${color}_frame%d.png -plays 0 -vf "fps=2" -f apng "bird${color}_animated.png"
     #ffmpeg -framerate 2 -i "bird${color}_frame%03d.png" -vf "fps=2" -plays 0 "bird${color}_animated.png"
 done
