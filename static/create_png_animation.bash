eef8c07e904e0bb7e01c7c6964db4a43eb39da17
Tim Schäfer <ts+code@rcmd.org>
2025-03-17 13:30:32 +0100

    FIX: fix ffmpeg script

--- static/create_png_animation.bash
+++ static/create_png_animation.bash
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for color in "gray" "green" "orange" "purple" "yellow"; do
+#for color in "gray" "green" "orange" "purple" "yellow"; do
+for color in "yellow"; do
     echo "Converting ${color} bird frames to to bird${color}_animated.png"
-    ffmpeg -framerate 2 -i bird${color}_frame%d.png -plays 0 bird${color}_animated.png
+    ffmpeg -framerate 2 -i bird${color}_frame%d.png -plays 0 -vf "fps=2" -f apng "bird${color}_animated.png"
+    #ffmpeg -framerate 2 -i "bird${color}_frame%03d.png" -vf "fps=2" -plays 0 "bird${color}_animated.png"
 done
