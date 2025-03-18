bb47411fd038d44ba3ae97a8dd40f4f25847d3db
Tim Schäfer <ts+code@rcmd.org>
2025-03-18 11:22:37 +0100

    CHG: remove default stuff

--- experiment.py
+++ experiment.py
@@ -29,6 +29,7 @@ class Exp(psynet.experiment.Experiment):
     label = "Graphics demo"
 
     # asset_storage = S3Storage("psynet-tests", "graphics")
+    imgdir = "/static"
 
     timeline = Timeline(
         NoConsent(),
@@ -38,263 +39,70 @@ class Exp(psynet.experiment.Experiment):
         ),
         ModularPage(
             "graphic",
-            prompt=GraphicPrompt(
-                text="This GraphicPrompt illustrates some of the different kinds of geometric objects.",
-                dimensions=[100, 100],
-                viewport_width=0.5,
-                frames=[
-                    Frame(
-                        [
-                            Path(
-                                "triangle",
-                                "M50,10 L10,50 L90,50 z",
-                                attributes={"fill": "red"},
-                            ),
-                            Circle(
-                                "circle",
-                                20,
-                                70,
-                                radius=5,
-                                attributes={"fill": "blue"},
-                                animations=[
-                                    Animation(
-                                        {"cx": 80}, duration=0.7, easing="bounce"
-                                    ),
-                                    Animation(
-                                        {"cx": 20}, duration=1.2, easing="ease-out"
-                                    ),
-                                ],
-                                loop_animations=True,
-                            ),
-                            Ellipse(
-                                "ellipse",
-                                80,
-                                80,
-                                radius_x=10,
-                                radius_y=5,
-                                attributes={"fill": "yellow"},
-                            ),
-                            Rectangle(
-                                "rectangle",
-                                50,
-                                85,
-                                width=80,
-                                height=5,
-                                attributes={"fill": "black"},
-                            ),
-                        ]
-                    )
-                ],
-            ),
-            time_estimate=5,
-        ),
-        ModularPage(
-            "graphic",
             prompt=Prompt(
-                text="This is an example of a GraphicControl. Click on one of the objects to continue to the next page.",
+                text="Leandra7 Singing Bird but with Animated PNG das reinfährt!",
             ),
             control=GraphicControl(
-                dimensions=[100, 100],
-                viewport_width=0.5,
+                dimensions=[200, 200],
+                viewport_width=1,
+
                 frames=[
                     Frame(
-                        [
-                            Text(
-                                "title",
-                                "PsyNet is great!",
-                                x=50,
-                                y=20,
-                                click_to_answer=True,
+                        [Image( "AnimatedPNG", media_id="Apng",
                                 persist=True,
-                                attributes={"fill": "blue"},
-                                animations=[
-                                    Animation({"fill": "red"}, duration=0.2),
-                                    Animation({"fill": "blue"}, duration=0.2),
-                                ],
-                                loop_animations=True,
+                                x=100,  # Adjust as necessary for centering
+                                y=100,  # Adjust as necessary for centering
+                                width=200,  # Set to the width of your viewport
+                                height=200,  # Set to the height of your viewport
+                                anchor_x=0.5,
+                                anchor_y=0.5,
                             ),
-                            Image(
-                                "logo",
-                                media_id="logo",
-                                x=10,
-                                y=40,
-                                width=75,
+
+                         Image( "background_image_id", media_id="background_image",
+                                persist=True,
+                                x=100,  # Adjust as necessary for centering
+                                y=100,  # Adjust as necessary for centering
+                                width=200,  # Set to the width of your viewport
+                                height=200,  # Set to the height of your viewport
+                                anchor_x=0.5,
+                                anchor_y=0.5,
+                            ),
+
+                            Image( "AnimatedPNGG", media_id="Apng",
+                                persist=True,
+                                x=50,  # Adjust as necessary for centering
+                                y=50,  # Adjust as necessary for centering
+                                width=100,  # Set to the width of your viewport
+                                height=100,  # Set to the height of your viewport
+                                anchor_x=0.5,
+                                anchor_y=0.5,
+                            ),
+
+                        Image( "birdONE", media_id="logorr",
+                                x=200,
+                                y=50,
+                                width=50,
                                 click_to_answer=True,
                                 animations=[
-                                    Animation(
-                                        {"x": 5, "y": 45, "width": 60}, duration=0.25
-                                    ),
-                                    Animation(
-                                        {"x": 15, "y": 50, "width": 90}, duration=0.25
-                                    ),
-                                    Animation(
-                                        {"x": 5, "y": 55, "width": 60}, duration=0.25
-                                    ),
-                                    Animation(
-                                        {"x": 10, "y": 60, "width": 76}, duration=0.25
-                                    ),
+                                    Animation({"x":75 }, duration=2 ),
+                                    #Animation( {"x": 100,"hide": False }, duration=2 ),
                                 ],
                             ),
                         ],
-                        duration=1,
                     ),
-                    Frame(
-                        [
-                            Image(
-                                "logo",
-                                media_id="logo",
-                                x=10,
-                                y=60,
-                                width=75,
-                                click_to_answer=True,
-                                animations=[
-                                    Animation(
-                                        {"y": 40, "transform": "r180"}, duration=0.5
-                                    ),
-                                    Animation(
-                                        {"y": 40, "transform": "r0"}, duration=0.5
-                                    ),
-                                ],
-                            )
-                        ],
-                        duration=1,
-                    ),
+
                 ],
                 loop=True,
-                media=MediaSpec(image=dict(logo="/static/logo.svg")),
+                media=MediaSpec(
+                    image=dict(
+                        logo=f"{imgdir}/logo.png",
+                        logorr="/static/Birdie-APNG.png",
+                        background_image="/static/World_1-FG.png",
+                        Apng="static/TransparentA.png")
+                    ),
+
             ),
             time_estimate=5,
         ),
         DebugResponsePage(),
-        ModularPage(
-            "audio",
-            prompt=GraphicPrompt(
-                text="This GraphicPrompt has synchronized audio.",
-                dimensions=[100, 100],
-                viewport_width=0.5,
-                frames=[
-                    Frame(
-                        [
-                            Text(
-                                "title",
-                                "Beer!",
-                                x=50,
-                                y=30,
-                                attributes={"fill": "blue"},
-                            )
-                        ],
-                        duration=1,
-                        audio_id="bier",
-                    ),
-                    Frame(
-                        [
-                            Text(
-                                "title", "Beer!", x=50, y=60, attributes={"fill": "red"}
-                            )
-                        ],
-                        duration=1,
-                        audio_id="bier",
-                    ),
-                ],
-                loop=True,
-                media=MediaSpec(audio={"bier": "/static/bier.wav"}),
-            ),
-            time_estimate=5,
-        ),
-        ModularPage(
-            "graphic",
-            prompt=GraphicPrompt(
-                text="This GraphicPrompt illustrates different font sizes.",
-                dimensions=[300, 100],
-                viewport_width=0.6,
-                frames=[
-                    Frame(
-                        [
-                            Text(
-                                "big",
-                                "Big text",
-                                x=150,
-                                y=30,
-                                attributes={"font-size": 30},
-                            ),
-                            Text(
-                                "small",
-                                "Small text",
-                                x=150,
-                                y=70,
-                                attributes={"font-size": 10},
-                            ),
-                        ]
-                    )
-                ],
-            ),
-            time_estimate=5,
-        ),
-        ModularPage(
-            "graphic",
-            prompt=GraphicPrompt(
-                text="This page contains both a GraphicPrompt and a GraphicControl.",
-                dimensions=[300, 100],
-                viewport_width=0.6,
-                frames=[
-                    Frame(
-                        [Text("question", "What's up? Click to answer.", x=150, y=50)]
-                    )
-                ],
-            ),
-            control=GraphicControl(
-                dimensions=[300, 100],
-                viewport_width=0.6,
-                frames=[
-                    Frame(
-                        [
-                            Text(
-                                "not_much",
-                                "Not much.",
-                                x=150,
-                                y=20,
-                                click_to_answer=True,
-                            ),
-                            Text("lots", "Lots.", x=150, y=80, click_to_answer=True),
-                        ]
-                    )
-                ],
-            ),
-            time_estimate=5,
-        ),
-        DebugResponsePage(),
-        ModularPage(
-            "audio_meter",
-            """
-            We will now demonstrate audio recording coupled with a Graphic Prompt.
-            First we need to enable your sound recorder.
-            """,
-            AudioMeterControl(calibrate=False),
-            time_estimate=5,
-        ),
-        ModularPage(
-            "audio_record",
-            prompt=GraphicPrompt(
-                text="This example shows how the GraphicPrompt can be used to trigger timing in the Control object.",
-                dimensions=[100, 100],
-                viewport_width=0.4,
-                frames=[
-                    Frame([Text("num_3", "3", 50, 50)], duration=1),
-                    Frame([Text("num_2", "2", 50, 50)], duration=1),
-                    Frame([Text("num_1", "1", 50, 50)], duration=1),
-                    Frame(
-                        [Text("sing", "Sing!", 50, 50)],
-                        duration=2,
-                        activate_control_response=True,
-                    ),
-                    Frame([Text("stop", "Stop.", 50, 50)], duration=None),
-                ],
-                prevent_control_response=True,
-            ),
-            control=AudioRecordControl(
-                duration=2,
-                bot_response_media="example_recordings/example_recording.wav",
-            ),
-            time_estimate=6,
-        ),
     )
